d0ee143b3e20158568f99a3a4c2c2cb39c23c17e
Nathanael A. Smith <nthepianoman@gmail.com>
2016-04-14 08:52:01 -0500

    Added empty files for echo server/client.

---
+++ tests/echo/echod.h
@@ -0,0 +1,9 @@
+/****************************************************************************
+ *            {echod.h}  => {the header file for the echo server}           *
+ *                                                                          *
+ * Assignment: Lab 10 - IPC sockets                                         *
+ * Author: Nathanael Smith and Austin Burdine                               *
+ * Computer: W002-13 and Macbook                                            *
+ *                                                                          *
+ ****************************************************************************/
+
