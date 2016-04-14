f22272d4c1e5d2c91a32ba5b75f8d116aa57e441
Austin Burdine <acburdine@csstudent-dev.olivet.edu>
2016-04-14 07:59:12 -0500

    Add header file

---
+++ header.c
@@ -0,0 +1,9 @@
+/****************************************************************************
+ *		   {{FILE}}  => {{DESCRIPTION}}         	            *
+ *                                                                          *
+ * Assignment: Lab 10 - IPC sockets                                         *
+ * Author: Nathanael Smith and Austin Burdine                               *
+ * Computer: W002-13 and Macbook                                            *
+ *                                                                          *
+ ****************************************************************************/
+
